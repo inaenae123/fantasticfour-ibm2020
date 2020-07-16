e9c2e424c68621c598503e7765e5d5c4bf7b7c1c
Park, Inae <inae.park@gatech.edu>
2020-07-16 09:48:19 -0400

    updated homepage

--- Client/src/Containers/Homepage/index.js
+++ Client/src/Containers/Homepage/index.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import styles from './styles.module.css';
 import {NavLink} from 'react-router-dom';
+import { Card } from 'semantic-ui-react';
 
 class Homepage extends Component{
+
     render(){
         const pStyle = {
             display: 'inline-block',
@@ -13,14 +15,38 @@ class Homepage extends Component{
             cursor: 'pointer'
         };
 
+        const items =  [
+            {
+                header: 'Get Help',
+                href: '/Tyler',
+                color: 'grey'
+              },
+              {
+                header: 'Donate',
+                href: '/Avery',
+                color:'grey'
+              },
+              {
+              header: 'Make a Difference',
+              href: '/Eon',
+              color:'grey'
+            },
+            {
+                header: 'Be More Informed',
+                href: '/Carmina',
+                color:'grey'
+              }
+        ];
+
         return(
             <div className="main" style={styles}>
                 <h1 style={{paddingTop: '20px'}}>Homepage</h1>
                 <h3>I want to...</h3>
-                <NavLink to='/Tyler'><p style={pStyle}>Get Help</p></NavLink>
+                <Card.Group centered items={items} />
+                {/* <NavLink to='/Tyler'><p style={pStyle}>Get Help</p></NavLink>
                 <NavLink to='/Avery'><p style={pStyle}>Donate</p></NavLink>
                 <NavLink to='/Eon'><p style={pStyle}>Make a Difference</p></NavLink>
-                <NavLink to='/Carmina'><p style={pStyle}>Be More Informed</p></NavLink>
+                <NavLink to='/Carmina'><p style={pStyle}>Be More Informed</p></NavLink> */}
             </div>
         )
     }
